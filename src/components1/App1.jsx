6b2a5544d12502ca28447ee982f5b0e626724281
Abhishek Ahlawat <74086878+AbhiAhlawat@users.noreply.github.com>
2022-11-11 22:19:16 +0530

    Add files via upload

---
+++ src/components1/App1.jsx
@@ -0,0 +1,14 @@
+import Header from './Header';
+import Main from './Main';
+
+function App1() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+
+    </div>
+  );
+}
+
+export default App1;
