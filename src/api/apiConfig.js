adf4f7a495c7c447d12fcc629a9e9cc579c316fd
Abhishek Ahlawat <74086878+AbhiAhlawat@users.noreply.github.com>
2022-11-11 22:13:09 +0530

    Delete src directory

--- src/api/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'ce2b7935785b78c67b44997a3b91739d',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
-
-export default apiConfig;
